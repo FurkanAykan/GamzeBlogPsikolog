e87c6c0538fc4b10eff65260e1b203ccbad37208
mehmetncr <mehmet.ncar@gmail.com>
2024-01-16 00:30:10 +0300

    add reply comment

--- GamzeBlogPsikolog/Services/CommentService.cs
+++ GamzeBlogPsikolog/Services/CommentService.cs
@@ -8,12 +8,14 @@ namespace GamzeBlogPsikolog.Services
     public class CommentService : ICommentService
     {
         private readonly IGenericRepostory<Comment> _commentRepo;
+        private readonly IGenericRepostory<ReplyComment> _replyCommentRepo;
         private readonly IMapper _mapper;
 
-        public CommentService(IGenericRepostory<Comment> commentRepo, IMapper mapper)
+        public CommentService(IGenericRepostory<Comment> commentRepo, IMapper mapper, IGenericRepostory<ReplyComment> replyCommentRepo)
         {
             _commentRepo = commentRepo;
             _mapper = mapper;
+            _replyCommentRepo = replyCommentRepo;
         }
 
         public async Task<string> AddComment(CommentViewModel comment)
@@ -35,9 +37,27 @@ namespace GamzeBlogPsikolog.Services
             
         }
 
+        public async Task<string> AddReplyComment(ReplyCommentViewModel replyComment)
+        {
+            try
+            {
+                ReplyComment newComment = _mapper.Map<ReplyComment>(replyComment);
+                newComment.ReplyCommentImage = "baseimage";
+                newComment.ReplyCommentStatus = false;
+                newComment.ReplyCreateDate = DateTime.Now;
+                await _replyCommentRepo.Add(newComment);
+                return "Ok";
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message;
+            }
+        }
+
         public async Task<List<CommentViewModel>> GetAllCommentByPostId(int postId)
         {
-            var commentlist=await _commentRepo.GetAll(x=>x.BlogPostId == postId,null,x=>x.ReplyComments);
+            var commentlist=await _commentRepo.GetAll(x=>x.BlogPostId == postId && x.CommentStatus==true,null,x=>x.ReplyComments);
             List<CommentViewModel> mappedCommentList = _mapper.Map<List<CommentViewModel>>(commentlist);
             return mappedCommentList;
         }
